77536b97b992441e6a38066e3fb1e3b64237ddac
Francisco Rubio <38331551+frrf@users.noreply.github.com>
2022-08-16 08:30:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// api url
 const api_url = "https://www.swapi.tech/api/";
 
 const getLinks = (api_url) => {
@@ -7,15 +6,13 @@ const getLinks = (api_url) => {
     .then((data) => directory(data))
 }
 
-
 const directory = (data) => {
   let result = data.result
   let categories = Object.keys(result)
-  let links = Object.values(result)
   
-  categories.forEach(x => {
-    console.log(x)
-    document.body.innerHTML += `<a id="${x}" href="#">${x}</a><br>`
+  categories.forEach(category => {
+    // console.log(x)
+    document.body.innerHTML += `<a id="${category}" href="#">${category}</a><br>`
   })
   forwardLink()
 }
@@ -25,27 +22,39 @@ getLinks(api_url)
 const forwardGetLinks = (api_url) => {
   fetch(api_url)
     .then((data) => data.json())
-    .then((data) => page(data))
+    .then((data) => selectCategory(data))
 }
 
+// const getObject = (object_url) => { 
+//   fetch(object_url).then((data) => data.json()).then((data) => console.log(data))
+// }
 
-const page = (data) => {
+// const getName = (object_url) => {
+//   fetch(object_url).then((data) => data.json()).then((data) => {return data})
+//   let result = x.result
+//   console.log('getname', x)
+//   let entityName = result.properties
+  
+//   console.log('-- entityName values',entityName)
+//   return `<p>${entityName}</p>`
+// }
+
+const selectCategory = (data) => {
   let result_var = Object.keys(data)[Object.keys(data).length-1] // accounts for films, uses result vs results
-  let result = data[result_var]
-
+  let result = data[result_var] // grabs 'results' or 'result' for films
   let entityNames = Object.values(result)
   
-  console.log('page',entityNames)
+  console.log('-- chosen category values',entityNames)
+
   document.body.innerHTML = ''
 
   result_var === 'results' 
-  ? entityNames.forEach(x => { document.body.innerHTML += `<p id="${x.uid}">${x.name}</p>` }) 
-  : entityNames.forEach(x => { document.body.innerHTML += `<p id="${x.uid}">${x.properties.title}</p>` }) 
-  
-
-  
-
-
+  ? entityNames.forEach(x => { 
+    document.body.innerHTML += `<p id="${x.uid}">${x.name}</p>`}) 
+  : entityNames.forEach(x => { 
+    document.body.innerHTML += `<h4 id="${x.uid}">${x.properties.title}</h4>`
+    // document.body.innerHTML += `<p>${getName(x.properties.characters[0])}</p>`//x.properties.characters
+  }) 
 }
 
 // adds event listener to directory a tags, makes new get request based on user selection
