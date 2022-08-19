c2e74baeeb9d08b8fb6419bf8b179f61be25ccc5
Francisco Rubio <38331551+frrf@users.noreply.github.com>
2022-08-19 07:41:40 -0700

    script rewrite
    decided to start from scratch again, realized I was going about this inefficiently and at times incorrectly. New understanding of promises so should be able to get something better working this way

--- script.js
+++ script.js
@@ -1,71 +1,22 @@
-const api_url = "https://www.swapi.tech/api/";
-
-const getLinks = (api_url) => {
-  fetch(api_url)
-    .then((data) => data.json())
-    .then((data) => directory(data))
+async function start() {
+  const response = await fetch("https://swapi.dev/api/")
+  const data = await response.json()
+  createDirectory(data)
 }
 
-const directory = (data) => {
-  let result = data.result
-  let categories = Object.keys(result)
-  
-  categories.forEach(category => {
-    // console.log(x)
-    document.body.innerHTML += `<a id="${category}" href="#">${category}</a><br>`
-  })
-  forwardLink()
-}
+start()
 
-getLinks(api_url)
+function createDirectory(data) {
+  let directory = document.getElementById("directory")
 
-const forwardGetLinks = (api_url) => {
-  fetch(api_url)
-    .then((data) => data.json())
-    .then((data) => selectCategory(data))
-}
-
-// const getObject = (object_url) => { 
-//   fetch(object_url).then((data) => data.json()).then((data) => console.log(data))
-// }
-
-// const getName = (object_url) => {
-//   fetch(object_url).then((data) => data.json()).then((data) => {return data})
-//   let result = x.result
-//   console.log('getname', x)
-//   let entityName = result.properties
-  
-//   console.log('-- entityName values',entityName)
-//   return `<p>${entityName}</p>`
-// }
-
-const selectCategory = (data) => {
-  let result_var = Object.keys(data)[Object.keys(data).length-1] // accounts for films, uses result vs results
-  let result = data[result_var] // grabs 'results' or 'result' for films
-  let entityNames = Object.values(result)
-  
-  console.log('-- chosen category values',entityNames)
-
-  document.body.innerHTML = ''
-
-  result_var === 'results' 
-  ? entityNames.forEach(x => { 
-    document.body.innerHTML += `<p id="${x.uid}">${x.name}</p>`}) 
-  : entityNames.forEach(x => { 
-    document.body.innerHTML += `<h4 id="${x.uid}">${x.properties.title}</h4>`
-    // document.body.innerHTML += `<p>${getName(x.properties.characters[0])}</p>`//x.properties.characters
-  }) 
-}
-
-// adds event listener to directory a tags, makes new get request based on user selection
-function forwardLink() {
-  const atags = document.querySelectorAll("a")
-
-  atags.forEach(tag => {
-    tag.addEventListener("click", () => {
-      let forward_api_url = api_url+tag.id
-      console.log('-- directory',tag.id)
-      forwardGetLinks(forward_api_url)
-    })
+  console.log('create dir function called from start')
+  console.log('data', Object.keys(data))
+  Object.keys(data).map((data) => {
+    directory.innerHTML += `
+      <a href="#" id="${data}">
+      ${data}<br>
+      </a>
+      `
   })
 }
+
