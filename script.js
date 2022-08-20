28259ec5138de655d0f2e93c6a94271942ca3f05
Francisco Rubio <38331551+frrf@users.noreply.github.com>
2022-08-19 18:45:37 -0700

    Completed displaying the first page of results from API based on category selection
    Next up, creating next and prev buttons and populating the https filled objects in  the category responses with object names/titles

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const api_url = 'https://swapi.dev/api/'
 
-async function start(api_url) {
+async function getDirectory(api_url) {
   const response = await fetch(api_url)
   const data = await response.json()
   createDirectory(data)
 }
 
-start(api_url)
+getDirectory(api_url)
 
 function createDirectory(data) {
   let directory = document.getElementById("directory")
@@ -20,13 +20,13 @@ function createDirectory(data) {
       </a>
       `
   })
-  
+
   const atags = document.querySelectorAll("a")
   atags.forEach((tag) => {
     tag.addEventListener("click", () => {
       // console.log('-- directory',tag.id)
-      let category_api_url = api_url+tag.id
-       getCategory(category_api_url)
+      let category_api_url = api_url + tag.id
+      getCategory(category_api_url)
     })
   })
 
@@ -41,5 +41,26 @@ async function getCategory(api_url) {
 }
 
 function createCategory(categoryData) {
-  console.log(categoryData)
+  const next = categoryData.next
+  const prev = categoryData.previous
+  let category = Object.values(categoryData.results)//.map((object) => {object})
+
+  let categoryHost = document.getElementById("categoryInfo")
+  categoryHost.innerHTML = ''
+
+  console.log('catgeory', categoryData)
+  category.forEach((item) => {
+    categoryHost.innerHTML += `<hr><ul>`
+    for(let key in item) {
+      if(typeof item[key] === 'object') {
+        // get titles or names of object members
+      } else if (key !== 'created' && key !== 'edited' && key !== 'url') {
+        categoryHost.innerHTML += `<li>${key}: ${item[key]}</li>`
+      }
+
+    }
+    categoryHost.innerHTML += `</ul>`
+ });
+
+  
 }
