9901694c302fb4591ec6573d56404d033d959601
Francisco Rubio <38331551+frrf@users.noreply.github.com>
2022-08-21 23:00:05 -0700

    Completes the basic requirement of assignment
    can navigate through all levels of the API
    
    Need to improve how listeners are applied, still struggling to create content before applying listeners to it. End up with content created after listener application

--- script.js
+++ script.js
@@ -22,67 +22,72 @@ function categoryEventListeners() {
   atags.forEach((tag) => {
     tag.addEventListener("click", () => {
       let category_api_url = api_url + tag.id
-      getCategories(category_api_url,tag.id)
+      getCategories(category_api_url, tag.id)
     })
   })
 }
 
 // fetches the initial set of selectable options belonging to the selected category from the directory
-async function getCategories(api_url,categoryName) {
+async function getCategories(api_url, categoryName) {
   const response = await fetch(api_url)
   const data = await response.json()
-  displayCategoryOptions(data,categoryName)
+  displayCategoryOptions(data, categoryName)
 }
 
 // displays selectable options from the SWAPI fetched data
-function displayCategoryOptions(data,category) {
+function displayCategoryOptions(data, category) {
   let categoryInfo = document.getElementById("categoryInfo")
+
   // Displays chosen category in a header 
-  categoryInfo.innerHTML = `<hr><h3 id="displayHeader">${category}</h3><section></section>` // insert next and prev buttons here
+  categoryInfo.innerHTML = `
+    <hr><h3 id="displayHeader">${category}</h3>
+    <section></section>
+    <div id="linkedOptions"></div>` // insert next and prev buttons here
   let dataDisplay = document.querySelector("main section")
   // add category options to HTML in <a> tags
   let options = data.results
   let optionName = ''
   options.forEach((option) => {
     // all but films use .name
-    (category !== 'films') ? optionName = option.name : optionName = option.title 
+    (category !== 'films') ? optionName = option.name : optionName = option.title
     dataDisplay.innerHTML += `<a href="#" class="category_option" name="${option.url}">${optionName}</a><br>`
   })
-  
+
   // gives event listeners
-  displayCategoryTraversal(data,category)
+  displayCategoryTraversal(data, category)
 
   // give <a> tags on click function event listeners
   let elementOptions = document.querySelectorAll("a[class=\"category_option\"]")
+  elementOptions.innerHTML = ''
   elementOptions.forEach(option => {
-    option.addEventListener("click", () => { 
+    option.addEventListener("click", () => {
       getOption(option.name)
     })
   })
 }
 
 // displays next and previous buttons to traverse sets of a category's available options to choose from
-function displayCategoryTraversal(data,category) {
+function displayCategoryTraversal(data, category) {
   let page = 0
-  if(data.previous === null) {page = 1} 
-  else if (data.next === null) {page = Math.ceil(data.count/10)} 
-  else {page = data.next[data.next.length-1] - 1}
+  if (data.previous === null) { page = 1 }
+  else if (data.next === null) { page = Math.ceil(data.count / 10) }
+  else { page = data.next[data.next.length - 1] - 1 }
 
   let dataDisplay = document.querySelector("main section")
-  dataDisplay.innerHTML += 
-  `<div id="categoryTraversal">
+  dataDisplay.innerHTML +=
+    `<div id="categoryTraversal">
   <button name="${data.previous}">previous</button>
   <label id="page">page: ${page}</label>
   <button name="${data.next}">next</button>
   </div>`
 
   let categoryTraversalButtons = document.querySelectorAll("button")
-  categoryTraversalButtons.forEach((button) => { 
+  categoryTraversalButtons.forEach((button) => {
     if (button.name === 'null') {
       button.disabled = true
     } else {
-      button.addEventListener("click", () => { 
-        getCategories(button.name,category)
+      button.addEventListener("click", () => {
+        getCategories(button.name, category)
       })
     }
   })
@@ -97,54 +102,54 @@ async function getOption(option_url) {
 
 // displays option's data
 function displayOptionData(data) {
+  
+  let linkedOptionsClear = document.getElementById("linkedOptions")
+  linkedOptionsClear.innerHTML = ''
+  // console.log(linkedOptionsClear)
+  
   let optionAttributes = Object.keys(data)
   let attributeValues = Object.values(data)
-  
+
 
   let dataHeader = document.getElementById("displayHeader")
-
   let dataDisplayWrapper = document.querySelector("main section")
   dataDisplayWrapper.innerHTML = `<ul id="optionData"></ul>`
   let baseDataDisplay = document.querySelector("main section ul")
-
-  attributeValues.forEach((value,key) => {
+  attributeValues.forEach((value, key) => {
     let keyName = optionAttributes[key]
 
     if (keyName === 'name') {
       dataHeader.innerHTML = `<strong>${data.name}</strong>`
-    } 
+    }
     else if (keyName === 'title') {
       dataHeader.innerHTML = `<strong>${data.title}</strong>`
     }
     else if (keyName != 'url') {
       if (typeof value === 'object' && value.length != 0) {
         value.forEach((link) => {
-          getOptionLinkedOptions(keyName,link)
-        })      
+        getOptionLinkedOptions(keyName, link)
+        })
       } else if (typeof value === 'string' && value.includes('https')) {
-          getOptionLinkedOptions(keyName,value)
+        getOptionLinkedOptions(keyName, value)
       } else {
-          if (value.length != 0 && keyName !== 'created' && keyName !== 'edited') 
+        if (value.length != 0 && keyName !== 'created' && keyName !== 'edited')
           baseDataDisplay.innerHTML += `<li>${keyName}: ${value}</li>`
       }
     }
   })
 }
 
-async function getOptionLinkedOptions(keyName,value) {
-    const response = await fetch(value)
-    const data = await response.json()
+async function getOptionLinkedOptions(keyName, value) {
+  const response = await fetch(value)
+  const data = await response.json()
+  await displayOptionLinkedOptions(keyName,data)
+  await getLinkedOption(data.url)
+}
 
-    // function to display option's single linked option 
-    displayOptionLinkedOptions(keyName,data)
-  }
-
-function displayOptionLinkedOptions(keyName,data) {
-  let displayArea = document.getElementById("categoryInfo")
+function displayOptionLinkedOptions(keyName, data) {
+  let displayArea = document.getElementById("linkedOptions")
   let categoryOptionsSection = document.querySelector(`main section ul#${keyName}`)
-  let optionName = ''
-  if (typeof data.name !== 'string') {optionName = data.title} else {optionName = data.name}
-  
+  if (typeof data.name !== 'string') { optionName = data.title } else { optionName = data.name }
   // if categoryOptionsSection is null, create categoryOptionsSection, append to if exists
   if (categoryOptionsSection === null) {
     displayArea.innerHTML += `
@@ -157,4 +162,20 @@ function displayOptionLinkedOptions(keyName,data) {
   } else {
     categoryOptionsSection.innerHTML += `<li><a href="#" name="${data.url}">${optionName}</a></li>`
   }
+}
+
+
+// gets option data when selected by user
+async function getLinkedOption(option_url) {
+  const response = await fetch(option_url)
+  const data = await response.json()
+  // let displayArea = document.getElementById("linkedOptions")
+  await displayLinkedOptionData(data,option_url)
+}
+
+function displayLinkedOptionData(data,url) {
+  let linkedObject = document.querySelectorAll(`a[name="${url}"]`)  
+  let target = linkedObject[0]
+  console.log('event listener on: ',target)
+  target.addEventListener("click",() => {displayOptionData(data)})
 }
